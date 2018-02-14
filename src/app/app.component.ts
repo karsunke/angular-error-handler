8ae4ebdb81001b01c24e4e77cf54372f6850ee49
Franziskus Karsunke <franziskus.karsunke.ext@pentasys.de>
2018-02-14 13:37:47 +0100

    error handler is not injected anymore.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,8 @@
-import {Component, ErrorHandler} from '@angular/core';
-import {RootErrorHandler} from './root.error-handler';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-
-  errorMessages: string[] = [];
-
-  constructor(private errorhandler: ErrorHandler) {
-  }
-
-  ngOnInit() {
-    (this.errorhandler as RootErrorHandler).logger = this;
-  }
-
-  public log(message: string) {
-    this.errorMessages.push(message);
-  }
 }
--- src/app/root.error-handler.ts
+++ src/app/root.error-handler.ts
@@ -1,16 +1,11 @@
 import {ErrorHandler, Injectable} from '@angular/core';
-import {AppComponent} from './app.component';
 
 @Injectable()
 export class RootErrorHandler extends ErrorHandler {
 
-  public logger: AppComponent;
 
   handleError(error: any): void {
-    if (this.logger) {
-      this.logger.log("Error handled by RootErrorHandler: " + error);
-      console.log("Error handled by RootErrorHandler: " + error)
-    }
+    console.log('Error handled by RootErrorHandler: ' + error);
   }
 
 }
